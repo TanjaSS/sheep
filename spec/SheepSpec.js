d79e4c65a17f7e0f2ab420b7477f33cca3a62e87
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-11-26 22:17:56 +0100

    Sheep has position

--- spec/SheepSpec.js
+++ spec/SheepSpec.js
@@ -2,12 +2,24 @@ describe("Sheep", function() {
     var sheep;
     
     beforeEach(function() {
-        sheep = new Sheep();
+        sheep = new Sheep(5, 10);
     });
     
     describe("#saySomething", function() {
         it("says 'beee'", function() {
             expect(sheep.saySomething()).toEqual("beee");
+        });
+    });
+    
+    describe("#getX", function() {
+        it("returns x", function() {
+            expect(sheep.getX()).toEqual(5);
+        });
+    });
+    
+    describe("#getY", function() {
+        it("returns y", function() {
+            expect(sheep.getY()).toEqual(10);
         });
     });
 });
--- src/Sheep.js
+++ src/Sheep.js
@@ -1,6 +1,16 @@
-function Sheep() {
+function Sheep(x, y) {
+    this.x = x;
+    this.y = y;
 }
 
 Sheep.prototype.saySomething = function() {
     return "beee";
+};
+
+Sheep.prototype.getX = function() {
+    return this.x;
+};
+
+Sheep.prototype.getY = function() {
+    return this.y;
 };
